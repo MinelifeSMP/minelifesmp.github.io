48d1b03c9eb3028e6074bfa384d460c8aaa31e4a
Craemon <121398546+Craemon@users.noreply.github.com>
2024-08-15 00:18:52 +0200

    change to webp

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 class SharedHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <header><a href="/index"><img src="../../assets/logo.png" alt="Minelife Logo" class="header_logo"></a></header>
+        <header><a href="/index"><img src="../../assets/logo.webp" alt="Minelife Logo" class="header_logo"></a></header>
     `
     }
 }
